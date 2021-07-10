b8038de0c5611d0456eaf9c3c4786fc5822e134c
0xxy <oksenyuk01@inbox.ru>
2021-07-10 15:58:46 +0300

    simple maths in JS

---
+++ lesson-2-1/main.js
@@ -0,0 +1,14 @@
+let user='John Doe';
+let student='Kate';
+
+user=student;
+
+console.log('user = ', user);
+
+let test = 1;
+test += 1;
+test = test + '1'; //21 
+test -= 1; //2
+test = !!(test); // true
+
+console.log('test = '+ test);
