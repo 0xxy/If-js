13a2d09df7302a4c505ff2da4562e1b7a57af014
0xxy <oksenyuk01@inbox.ru>
2021-07-14 15:00:16 +0300

    use const

--- lesson-2-2/main.js
+++ lesson-2-2/main.js
@@ -1,4 +1,4 @@
-let array = [2, 3, 5, 8];
+const array = [2, 3, 5, 8];
 let result = 1;
 for(let i = 0; i < array.length; i++){
   result *= array[i];
@@ -6,16 +6,15 @@ for(let i = 0; i < array.length; i++){
 
 console.log(`result= ${result}`);
 
-let array2 = [2, 5, 8, 15, 0, 6, 20, 3];
+const array2 = [2, 5, 8, 15, 0, 6, 20, 3];
 for(let i = 0; i < array2.length; i++){
   if(array2[i] > 5 && array2[i] < 10){
     console.log(array2[i]);
   }
 }
 
-let array3 = [2, 5, 8, 15, 0, 6, 20, 3];
 for(let i = 0; i < array2.length; i++){
-  if(array3[i] % 2 === 0){
-    console.log(array3[i]);
+  if(array2[i] % 2 === 0){
+    console.log(array2[i]);
   }
 }
---
+++ lesson-3/main.js
@@ -0,0 +1,4 @@
+let i = 1;
+ if(i >= 1 && i <=100){
+   console.log(i);
+ }
