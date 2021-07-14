f4162b5eb009a0dc39b9967f2fa3ce324e7c1740
0xxy <oksenyuk01@inbox.ru>
2021-07-14 14:36:15 +0300

    strict equals

--- lesson-2-2/main.js
+++ lesson-2-2/main.js
@@ -15,7 +15,7 @@ for(let i = 0; i < array2.length; i++){
 
 let array3 = [2, 5, 8, 15, 0, 6, 20, 3];
 for(let i = 0; i < array2.length; i++){
-  if(array3[i] % 2 == 0){
+  if(array3[i] % 2 === 0){
     console.log(array3[i]);
   }
 }
