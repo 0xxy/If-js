7a04abaff87daf5c204ec0dc4f7679650c99d86c
0xxy <oksenyuk01@inbox.ru>
2021-07-27 16:31:44 +0300

    change 2 task

--- lesson-4/main.js
+++ lesson-4/main.js
@@ -7,40 +7,20 @@ function sum(a){
 console.log(sum(5)(2));
 
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-const text1 = document.getElementById('text1');
-const text2 = document.getElementById('text2');
-const text3 = document.getElementById('text3');
+const text = document.getElementsByTagName('p');
 
-function changeParagraphColor1(){
-  for (let i = 0; i <= colors.length; i++) {
-    text1.style.color = colors[i];
-  }
-
-  if(i == colors.length){
-    i = 0;
-  }
-}
-
-function changeParagraphColor2(){
-  for (let i = 0; i <= colors.length; i++) {
-    text2.style.color = colors[i];
-  }
-
-  if(i == colors.length){
-    i = 0;
+let changeParagraphColor = function(){
+  let i = 0;
+  return function(){
+    this.style.color = colors[i];
+    i++;
+    
+    if (i == colors.length){
+      i = 0;
+    }
   }
 }
 
-function changeParagraphColor3(){
-  for (let i = 0; i <= colors.length; i++) {
-    text3.style.color = colors[i];
-  }
-
-  if(i == colors.length){
-    i = 0;
-  }
+for (let i = 0; i < text.length; i++){
+  text[i].addEventListener('click', changeParagraphColor());
 }
-
-text1.addEventListener('click', changeParagraphColor1.bind(null, text1));
-text2.addEventListener('click', changeParagraphColor2.bind(null, text2));
-text3.addEventListener('click', changeParagraphColor3.bind(null, text3));
