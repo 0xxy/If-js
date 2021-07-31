56814139145efb888eb4bd5cf19510efa77f947a
0xxy <oksenyuk01@inbox.ru>
2021-07-31 14:09:35 +0300

    Edit 2 task, use by Id and it is work!

--- lesson-4/main.js
+++ lesson-4/main.js
@@ -7,21 +7,22 @@ function sum(a){
 console.log(sum(5)(2));
 
 const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-const text1 = document.getElementsById('text1');
-const text2 = document.getElementsById('text2');
-const text3 = document.getElementsById('text3');
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
 
-const changeColor = () =>
-let count = 0;
-return () => {
-  this.style.color = colors[count];
-  count++;
-  
-  if (count == colors.length){
-    count = 0;
+const changeColor = (textElem) => {
+  let count = 0;
+  return () => {
+    textElem.style.color = colors[count];
+    count++;
+    
+    if (count === colors.length){
+      count = 0;
+    }
   }
 }
 
-text1.addEventListener('click', changeColor.bind(null, text1));
-text2.addEventListener('click', changeColor.bind(null, text2));
-text3.addEventListener('click', changeColor.bind(null, text3));
+text1.addEventListener('click', changeColor(text1));
+text2.addEventListener('click', changeColor(text2));
+text3.addEventListener('click', changeColor(text3));
