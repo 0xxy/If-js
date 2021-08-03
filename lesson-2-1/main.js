f54a8237836d47af31121ae2612d67d9a058928f
0xxy <83834639+0xxy@users.noreply.github.com>
2021-08-03 12:05:03 +0300

    simple maths in JS (#1)
    * simple maths in JS
    
    * edit with all console

---
+++ lesson-2-1/main.js
@@ -0,0 +1,19 @@
+let user='John Doe';
+console.log('user = '+ user);
+
+let student='Kate';
+console.log('student = '+ student);
+
+user=student; // Kate
+console.log('user = '+ user);
+
+let test = 1;
+test += 1;
+test = test + '1'; //21 
+console.log(test);
+
+test -= 1; //20
+console.log(test);
+
+test = !!(test); // true
+console.log(test);
