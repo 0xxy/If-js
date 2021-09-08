a95f32b4e3758342bb8ff65fbd375630abef986b
0xxy <oksenyuk01@inbox.ru>
2021-09-08 21:46:46 +0300

    add select for age of child

--- lesson-12/index.js
+++ lesson-12/index.js
@@ -1,7 +1,9 @@
 const otherInfo = document.getElementsByClassName('other-info');
 const selectForOtherInfo = document.getElementsByClassName('select-for-other-info');
+const ageOfChild = document.getElementsByClassName('age-of-child');
 
 selectForOtherInfo[0].style.display = 'none';
+ageOfChild[0].style.display = 'none';
 
 otherInfo[0].onclick = function() {
   selectForOtherInfo[0].style.display = 'block';
@@ -18,9 +20,76 @@ const counterAdults = document.getElementsByClassName('counter-adults');
 const counterChildren = document.getElementsByClassName('counter-children');
 const counterRooms = document.getElementsByClassName('counter-rooms');
 
-function decreaseAdults () {
-  let numberCounterAdults = Number(counterAdults[0].innerHTML);
-  numberCounterAdults;
+const selectAgeChild = document.getElementsByClassName('select-age-child');
+
+function decreaseAdults (event) {
+  event.preventDefault();
+
+  if (Number(counterAdults[0].innerHTML)) {
+    counterAdults[0].innerHTML = Number(counterAdults[0].innerHTML) - 1;
+  }
 }
 
 minuseInAdults[0].addEventListener('click', decreaseAdults);
+
+function increaseAdults (event) {
+  event.preventDefault();
+
+  if (Number(counterAdults[0].innerHTML) <= 29) {
+    counterAdults[0].innerHTML = Number(counterAdults[0].innerHTML) + 1;
+  }
+}
+
+pluseInAdults[0].addEventListener('click', increaseAdults);
+
+function increaseChildren (event) {
+  event.preventDefault();
+
+  ageOfChild[0].style.display = 'block';
+
+  if (Number(counterChildren[0].innerHTML) <= 9) {
+    counterChildren[0].innerHTML = Number(counterChildren[0].innerHTML) + 1;
+    selectAgeChild.append;
+    const newSelect = document.createElement('select');
+
+    for(let i = 0; i <= 17; i++) {
+      newSelect.options[newSelect.options.length]= new Option(`${i} years old`, `${i + 1}`);
+    }
+
+    ageOfChild[0].appendChild(newSelect);
+  }
+}
+
+pluseInChildren[0].addEventListener('click', increaseChildren);
+
+function decreaseChildren (event) {
+  event.preventDefault();
+
+  if (Number(counterChildren[0].innerHTML)) {
+    counterChildren[0].innerHTML = Number(counterChildren[0].innerHTML) - 1;
+    
+    ageOfChild[0].removeChild();
+  }
+}
+
+minuseInChildren[0].addEventListener('click', decreaseChildren);
+
+function decreaseRooms (event) {
+  event.preventDefault();
+
+  if (Number(counterRooms[0].innerHTML)) {
+    counterRooms[0].innerHTML = Number(counterRooms[0].innerHTML) - 1;
+  }
+}
+
+minuseInRooms[0].addEventListener('click', decreaseRooms);
+
+function increaseRooms (event) {
+  event.preventDefault();
+
+  if (Number(counterRooms[0].innerHTML) <= 29) {
+    counterRooms[0].innerHTML = Number(counterRooms[0].innerHTML) + 1;
+  }
+}
+
+pluseInRooms[0].addEventListener('click', increaseRooms);
