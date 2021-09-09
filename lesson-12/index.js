5b3cfa28eada06179f50cb83badbb99634b76575
0xxy <oksenyuk01@inbox.ru>
2021-09-09 22:12:12 +0300

    remove [0]

--- lesson-12/index.js
+++ lesson-12/index.js
@@ -1,95 +1,99 @@
-const otherInfo = document.getElementsByClassName('other-info');
-const selectForOtherInfo = document.getElementsByClassName('select-for-other-info');
-const ageOfChild = document.getElementsByClassName('age-of-child');
+const otherInfo = document.getElementsByClassName('other-info')[0];
+const selectForOtherInfo = document.getElementsByClassName('select-for-other-info')[0];
+const ageOfChild = document.getElementsByClassName('age-of-child')[0];
 
-selectForOtherInfo[0].style.display = 'none';
-ageOfChild[0].style.display = 'none';
+selectForOtherInfo.style.display = 'none';
+ageOfChild.style.display = 'none';
 
-otherInfo[0].onclick = function() {
-  selectForOtherInfo[0].style.display = 'block';
+otherInfo.onclick = function() {
+  selectForOtherInfo.style.display = 'block';
 }
 
-const minuseInAdults = document.getElementsByClassName('minuse-in-adults');
-const minuseInChildren = document.getElementsByClassName('minuse-in-children');
-const minuseInRooms = document.getElementsByClassName('minuse-in-rooms');
-const pluseInAdults = document.getElementsByClassName('pluse-in-adults');
-const pluseInChildren = document.getElementsByClassName('pluse-in-children');
-const pluseInRooms = document.getElementsByClassName('pluse-in-rooms');
+const minuseInAdults = document.getElementsByClassName('minuse-in-adults')[0];
+const minuseInChildren = document.getElementsByClassName('minuse-in-children')[0];
+const minuseInRooms = document.getElementsByClassName('minuse-in-rooms')[0];
+const pluseInAdults = document.getElementsByClassName('pluse-in-adults')[0];
+const pluseInChildren = document.getElementsByClassName('pluse-in-children')[0];
+const pluseInRooms = document.getElementsByClassName('pluse-in-rooms')[0];
 
-const counterAdults = document.getElementsByClassName('counter-adults');
-const counterChildren = document.getElementsByClassName('counter-children');
-const counterRooms = document.getElementsByClassName('counter-rooms');
+const counterAdults = document.getElementsByClassName('counter-adults')[0];
+const counterChildren = document.getElementsByClassName('counter-children')[0];
+const counterRooms = document.getElementsByClassName('counter-rooms')[0];
 
 const selectAgeChild = document.getElementsByClassName('select-age-child');
 
 function decreaseAdults (event) {
   event.preventDefault();
 
-  if (Number(counterAdults[0].innerHTML)) {
-    counterAdults[0].innerHTML = Number(counterAdults[0].innerHTML) - 1;
+  if (Number(counterAdults.innerHTML)) {
+    counterAdults.innerHTML = Number(counterAdults.innerHTML) - 1;
   }
 }
 
-minuseInAdults[0].addEventListener('click', decreaseAdults);
+minuseInAdults.addEventListener('click', decreaseAdults);
 
 function increaseAdults (event) {
   event.preventDefault();
 
-  if (Number(counterAdults[0].innerHTML) <= 29) {
-    counterAdults[0].innerHTML = Number(counterAdults[0].innerHTML) + 1;
+  if (Number(counterAdults.innerHTML) <= 29) {
+    counterAdults.innerHTML = Number(counterAdults.innerHTML) + 1;
   }
 }
 
-pluseInAdults[0].addEventListener('click', increaseAdults);
+pluseInAdults.addEventListener('click', increaseAdults);
 
 function increaseChildren (event) {
   event.preventDefault();
 
-  ageOfChild[0].style.display = 'block';
+  const newSelect = document.createElement('select');
+  newSelect.classList.add('new-select-js');
 
-  if (Number(counterChildren[0].innerHTML) <= 9) {
-    counterChildren[0].innerHTML = Number(counterChildren[0].innerHTML) + 1;
+  ageOfChild.style.display = 'block';
+
+  if (Number(counterChildren.innerHTML) <= 9) {
+    counterChildren.innerHTML = Number(counterChildren.innerHTML) + 1;
     selectAgeChild.append;
-    const newSelect = document.createElement('select');
 
     for(let i = 0; i <= 17; i++) {
       newSelect.options[newSelect.options.length]= new Option(`${i} years old`, `${i + 1}`);
     }
 
-    ageOfChild[0].appendChild(newSelect);
+    ageOfChild.appendChild(newSelect);
   }
 }
 
-pluseInChildren[0].addEventListener('click', increaseChildren);
+pluseInChildren.addEventListener('click', increaseChildren);
 
 function decreaseChildren (event) {
   event.preventDefault();
 
-  if (Number(counterChildren[0].innerHTML)) {
-    counterChildren[0].innerHTML = Number(counterChildren[0].innerHTML) - 1;
+
+  if (Number(counterChildren.innerHTML)) {
+    counterChildren.innerHTML = Number(counterChildren.innerHTML) - 1;
     
-    ageOfChild[0].removeChild();
+    const newSelect = document.getElementsByClassName('new-select-js');
+    ageOfChild.removeChild(newSelect);
   }
 }
 
-minuseInChildren[0].addEventListener('click', decreaseChildren);
+minuseInChildren.addEventListener('click', decreaseChildren);
 
 function decreaseRooms (event) {
   event.preventDefault();
 
-  if (Number(counterRooms[0].innerHTML)) {
-    counterRooms[0].innerHTML = Number(counterRooms[0].innerHTML) - 1;
+  if (Number(counterRooms.innerHTML)) {
+    counterRooms.innerHTML = Number(counterRooms.innerHTML) - 1;
   }
 }
 
-minuseInRooms[0].addEventListener('click', decreaseRooms);
+minuseInRooms.addEventListener('click', decreaseRooms);
 
 function increaseRooms (event) {
   event.preventDefault();
 
-  if (Number(counterRooms[0].innerHTML) <= 29) {
-    counterRooms[0].innerHTML = Number(counterRooms[0].innerHTML) + 1;
+  if (Number(counterRooms.innerHTML) <= 29) {
+    counterRooms.innerHTML = Number(counterRooms.innerHTML) + 1;
   }
 }
 
-pluseInRooms[0].addEventListener('click', increaseRooms);
+pluseInRooms.addEventListener('click', increaseRooms);
