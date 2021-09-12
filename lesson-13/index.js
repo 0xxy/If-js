ac1ff8b9b3eb4aa6dc5e3c525494cb9e31a00548
0xxy <oksenyuk01@inbox.ru>
2021-09-12 22:16:17 +0300

    fetch request

---
+++ lesson-13/index.js
@@ -0,0 +1,21 @@
+fetch('https://fe-student-api.herokuapp.com/api/hotels/popular') 
+  .then(response => response.json())
+  .then(function(data) {
+
+    const hotelsInform = document.getElementById('hotels-inform');
+
+    data.forEach(function(elem) {
+      const content = `
+      <a href="#"><img class='hotels__inform-img' src="${elem.imageUrl}" alt="${elem.name}"></a>
+      <p class='hotels__inform-name'>${elem.name}</p>
+      <p class='hotels__inform-city'>${elem.city}</p>
+      <p class='hotels__inform-city'>${elem.country}</p>
+    `;
+
+    hotelsInform.innerHTML += content;
+    });
+  })
+  
+  .catch(function(err) {
+    alert('Fetch problem: ' + err.message);
+  });
