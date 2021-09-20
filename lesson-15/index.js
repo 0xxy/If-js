ce27ee59e1210f72412a42acc18aae93544f7fca
0xxy <oksenyuk01@inbox.ru>
2021-09-20 15:28:56 +0300

    add async/await

--- lesson-15/index.js
+++ lesson-15/index.js
@@ -1,12 +1,12 @@
 const formEl = document.getElementById('form');
 const urlHotelsContentFile = 'https://fe-student-api.herokuapp.com/api/file';
 
-const eventsWithData = (event) => {
+const eventsWithData = async(event) => {
   event.preventDefault();
 
   const formData = new FormData(formEl);
 
-  fetch(urlHotelsContentFile, {
+  await fetch(urlHotelsContentFile, {
     method: 'POST',
     body: formData,
   }).then(response => response.json())
