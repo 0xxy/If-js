61c5f3a82002e09e02a0e20d3014716cd6f11b1a
0xxy <83834639+0xxy@users.noreply.github.com>
2021-09-22 20:47:50 +0300

    Lesson-15 Working with files (#15)
    * submit with formData
    
    * add const & tabs
    
    * add function & edit const
    
    * with arrow
    
    * add async/await
    
    * add catch
    
    * delete tab
    
    * delete enter

---
+++ lesson-15/index.js
@@ -0,0 +1,17 @@
+const formEl = document.getElementById('form');
+const urlHotelsContentFile = 'https://fe-student-api.herokuapp.com/api/file';
+
+const eventsWithData = async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(formEl);
+
+  await fetch(urlHotelsContentFile, {
+    method: 'POST',
+    body: formData,
+  }).then(response => response.json())
+  .then(data => console.log(data))
+  .catch((err) => console.log('Fetch problem: ' + err.message))
+}
+
+formEl.addEventListener('submit', eventsWithData);
